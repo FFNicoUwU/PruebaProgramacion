1f408ee6c8ca3339025d0aa4d0a3df8852bb2dde
unknown <javier.matamala252@gmail.com>
2022-10-22 02:02:32 -0300

    crear clase Bebestible

---
+++ Minimarket/src/minimarket/Bebestible.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minimarket;
+
+/**
+ *
+ * @author Javier
+ */
+public class Bebestible extends Producto{
+    //Atributos
+    private String tipoBebestibles;
+    //Contructores
+    public Bebestible(String nombre, String color, int precio, String tipoBebestibles) {
+        super(nombre, color, precio);
+        this.tipoBebestibles=tipoBebestibles;
+        //Getter And Setter
+        
+    }
+
+    public String getTipoBebestibles() {
+        return tipoBebestibles;
+    }
+
+    public void setTipoBebestibles(String tipoBebestibles) {
+        this.tipoBebestibles = tipoBebestibles;
+    }
+}
